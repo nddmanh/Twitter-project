f4de6fca112ee75c7f2a5792a7b038062ca5946c
nddmanh <manhnd.ilec@gmail.com>
2021-07-05 13:55:10 +0700

    [1.0.0] Install Nodemon, Middlware, Cors, Body Parser.

--- Back-end/server.js
+++ Back-end/server.js
@@ -1,12 +1,22 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
+
+// Cors
+app.use(cors());
+
+// Body Parser
+app.use(express.json());
+
 const PORT = 5000;
 app.get('/', (req, res, next) => {
     res.status(200).json({
         status: 'success',
         data: {
-            posts: []
+            posts: [{
+                content: "hello world!!!!"
+            }]
         }
     })
 })
