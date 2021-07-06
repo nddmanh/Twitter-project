0e1746e5cf5160713242f5c1b4005a184b48ead6
nddmanh <manhnd.ilec@gmail.com>
2021-07-06 11:12:58 +0700

    [1.0.0] CRUD Post.

---
+++ Back-end/controllers/postController.js
@@ -0,0 +1,57 @@
+const Post = require('./../models/Post');
+
+// Get all Posts
+exports.getAllPosts = async (req, res, next) => {
+    try {
+        const posts = await Post.find({}).populate('author');
+        res.status(200).json({
+            status: 'success',
+            result: posts.length,
+            data: {posts}
+        })
+    } catch (error) {
+        res.json(error);
+    }
+};
+
+// Create one post
+exports.createOnePost = async (req, res, next) => {
+    try {
+        const {userId} = req.user;
+        const post = await Post.create({...req.body, author: userId});
+        res.status(200).json({
+            status: 'success',
+            data: {post}
+        })
+    } catch (error) {
+        res.json(error);
+    }
+};
+
+// Update one post
+exports.updateOnePost = async (req, res, next) => {
+    try {
+        const {postId} = req.params;
+        const post = await Post.findByIdAndUpdate(postId, {...req.body}, {new: true, runValidator: true});
+        res.status(200).json({
+            status: 'success',
+            data: {post}
+        })
+    } catch (error) {
+        res.json(error);
+    }
+};
+
+// Delete one post
+exports.deleteOnePost = async (req, res, next) => {
+    try {
+        const {postId} = req.params;
+        await Post.findByIdAndDelete(postId);
+        res.status(200).json({
+            status: 'success',
+            message: 'Post has been deleted.'
+        })
+    } catch (error) {
+        res.json(error);
+    }
+};
